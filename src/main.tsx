a6a217f66d6a35b0e17c8ca83539f3c01f659681
beliberda <anton.nurgaliev@mail.ru>
2024-06-07 17:45:39 +1000

    store

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
+import Store from '@/components/store/store'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './variables.css'
 
+const store = new Store()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    
     <App />
   </React.StrictMode>,
 )
